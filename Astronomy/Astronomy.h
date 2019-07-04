7922b978d6079b2e521996a3f681f7163ecfe93e
Li Linfeng <lilinfeng@tp-link.com.cn>
2019-07-04 14:25:51 +0800

    use framework

---
+++ Astronomy/Astronomy.h
@@ -0,0 +1,19 @@
+//
+//  Astronomy.h
+//  Astronomy
+//
+//  Created by Li Linfeng on 4/7/2019.
+//  Copyright © 2019 Li Linfeng. All rights reserved.
+//
+
+#import "Astro.h"
+
+//! Project version number for Astronomy.
+FOUNDATION_EXPORT double AstronomyVersionNumber;
+
+//! Project version string for Astronomy.
+FOUNDATION_EXPORT const unsigned char AstronomyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Astronomy/PublicHeader.h>
+
+
